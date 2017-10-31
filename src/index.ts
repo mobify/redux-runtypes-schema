5aeeab9916be5f4db4d8d33a03ddb93a7dfdd8ff
Jeremy Wiebe <jeremy@mobify.me>
2017-10-31 14:36:57 -0700

    Add copyright headers

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+/* * *  *  * *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * */
+/* Copyright (c) 2017 Mobify Research & Development Inc. All rights reserved. */
+/* * *  *  * *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * */
+
 interface Action {
     type: string,
     payload: any,
